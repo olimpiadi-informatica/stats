ccd0e1de39f2b81d804c0cba1ed0582444b5f0f3
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-11-27 15:06:58 +0100

    Remove old frontend

--- frontend/src/components/Loading.tsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class Loading extends Component {
-  render() {
-    return <span>Loading...</span>;
-  }
-}
--- frontend/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-
-import Routes from "./routes";
-import "./styles/theme.css";
-
-render(<Routes />, document.getElementById("root"));
