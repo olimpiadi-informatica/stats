881cb5c89100f53eb772c0fd63c7764b8cd2f168
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-11-28 23:20:45 +0100

    Search bar

--- frontend/components/layout/search.tsx
+++ frontend/components/layout/search.tsx
@@ -1,13 +1,32 @@
 import styles from "./search.module.scss";
 import { FormControl, Button } from "react-bootstrap";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export function Search() {
+  const [q, setQ] = useState<string>("");
+  const router = useRouter();
+
   return (
-    <div>
-      <FormControl placeholder="Search" className={styles.searchBar} />
-      <Button className={styles.searchBtn} variant="outline-success">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        router.push(`/search?q=${encodeURIComponent(q)}`);
+      }}
+    >
+      <FormControl
+        placeholder="Search"
+        className={styles.searchBar}
+        value={q}
+        onChange={(e) => setQ(e.target.value)}
+      />
+      <Button
+        className={styles.searchBtn}
+        variant="outline-success"
+        type="submit"
+      >
         Search
       </Button>
-    </div>
+    </form>
   );
 }
