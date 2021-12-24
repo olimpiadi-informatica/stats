5192abd9b9e1a64f5f9b3154f5bb29ea7146170f
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-12-24 18:55:58 +0100

    Nicer build script

--- utils/docker_build.sh
+++ utils/docker_build.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 
+usage() {
+  echo "Usage: $0 [-p]"
+  echo "  -p      Push the built images"
+  echo "  -t tag  Override the tag"
+}
+
 if [ ! -f backend/docker/Dockerfile ]; then
+  usage
   echo "Error: run this script from the repo root"
   exit 1
 fi
 
-# compute the image tag
+PUSH=0
 TAG=$(date +%Y%m%d)
+
+while getopts "pht:" opt; do
+  case "$opt" in
+    p)
+      PUSH=1
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    t)
+      TAG="$OPTARG"
+      ;;
+    *)
+      usage
+      exit 1
+  esac
+done
+
+# compute the image tag
 echo "Image tag is: $TAG"
 
 docker build \
@@ -18,3 +45,8 @@ docker build \
   -t "ghcr.io/algorithm-ninja/oii-stats-frontend:$TAG" \
   -f frontend/docker/Dockerfile \
   frontend
+
+if [ "$PUSH" = 1 ]; then
+  docker push "ghcr.io/algorithm-ninja/oii-stats-backend:$TAG"
+  docker push "ghcr.io/algorithm-ninja/oii-stats-frontend:$TAG"
+fi
