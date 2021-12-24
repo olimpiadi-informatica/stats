488105b4d200719dc11886c0b395ad0d17171068
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-12-24 18:30:22 +0100

    Fix favicon

--- frontend/components/layout/layout.tsx
+++ frontend/components/layout/layout.tsx
@@ -18,8 +18,16 @@ export function Layout({ children }: Props) {
           name="description"
           content="OII stats is a platform that was created to provide essential but reliable statistics on the progress of the national competitions of the Italian IT Olympics."
         />
-        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/images/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <div>
         <Link href={"/"}>
