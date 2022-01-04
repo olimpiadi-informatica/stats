f4530cddbe9646737f89dc442957676258d45798
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2022-01-04 19:34:03 +0100

    Infinite scrolling in the hall of fame

--- frontend/components/contestantList/contestantList.tsx
+++ frontend/components/contestantList/contestantList.tsx
@@ -1,8 +1,8 @@
 import { ContestantList } from "lib/remote/user";
 import { useState } from "react";
-import { Button, ListGroup } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
 import { ContestantListItem } from "./contestantListItem";
-import styles from "./contestantList.module.scss";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 type Props = {
   users: ContestantList;
@@ -13,22 +13,28 @@ const ITEMS_PER_PAGE = 20;
 export function ContestantList({ users }: Props) {
   const [numPages, setNumPages] = useState<number>(1);
 
+  const items = users.users.slice(0, ITEMS_PER_PAGE * numPages);
+  const hasMore = ITEMS_PER_PAGE * numPages < users.users.length;
+
   return (
-    <>
+    <InfiniteScroll
+      dataLength={items.length}
+      next={() => setNumPages(numPages + 1)}
+      hasMore={hasMore}
+      loader={null}
+      endMessage={
+        <p>
+          Not bad, you scrolled through all {users.users.length} contestants!
+        </p>
+      }
+    >
       <ListGroup variant="flush">
-        {users.users.slice(0, ITEMS_PER_PAGE * numPages).map((user) => (
+        {items.map((user) => (
           <ListGroup.Item key={user.contestant.id}>
             <ContestantListItem user={user} />
           </ListGroup.Item>
         ))}
       </ListGroup>
-      <Button
-        variant="outline-secondary"
-        onClick={() => setNumPages(numPages + 1)}
-        className={styles.loadMore}
-      >
-        Load more
-      </Button>
-    </>
+    </InfiniteScroll>
   );
 }
