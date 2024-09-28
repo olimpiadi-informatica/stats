41f44872be7dfda6c1793a243ee96cbec94206bc
edomora97 <edomora97@users.noreply.github.com>
2024-09-28 12:14:48 +0000

    deploy: eba4bf8b5c99ccb5dee81348b3335a64c1a483ab

---
+++ _next/static/chunks/app/tasks/page-4510443c1b782f80.js
@@ -0,0 +1,2 @@
+(globalThis.webpackChunk_N_E=globalThis.webpackChunk_N_E||[]).push([[7289,1984,8096,2299,4687],{97646:(e,s,h)=>{Promise.resolve().then(h.bind(h,53016)),Promise.resolve().then(h.t.bind(h,231,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[5654,516,2971,7023,1744],()=>s(97646)),_N_E=e.O()}]);
+//# sourceMappingURL=page-4510443c1b782f80.js.map
